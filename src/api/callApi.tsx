3d8410c2a850a86791e53d1f8307c61fb12b9ec9
kiran katti <kirankatti550@gmail.com>
2024-10-08 12:05:01 +0530

    chenged url

--- src/api/callApi.tsx
+++ src/api/callApi.tsx
@@ -20,8 +20,8 @@ export const makeApi = async (
 
 		const config: AxiosRequestConfig = {
 			method,
-			url: `http://localhost:7000${endpoint}`,
-			// url: `https://pajiweb.onrender.com${endpoint}`,
+			// url: `http://localhost:7000${endpoint}`,
+			url: `https://new-palji-backend-1.onrender.com${endpoint}`,
 			// url: `https://sk-backend-uvv5.onrender.com${endpoint}`,
 
 			headers,
