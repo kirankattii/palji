89845ac3bab06689ac1b49f0da48869c10a734d5
kiran katti <kirankatti550@gmail.com>
2024-10-09 17:08:52 +0530

    done

--- src/components/navSearchList/NavSearchList.jsx
+++ src/components/navSearchList/NavSearchList.jsx
@@ -30,7 +30,48 @@
 // 	)
 // }
 
+
 // export default NavSearchList
+
+
+
+
+// import React from "react";
+// import "./navSearchList.css";
+// import { Link } from "react-router-dom";
+
+// const NavSearchList = ({ product, clearSearchInput, input, isLoading }) => {
+// 	return (
+// 		<div className="nav-search-list">
+// 			{isLoading ? (
+// 				<div className="loading">Loading...</div>
+// 			) : input && product.length === 0 ? (
+// 				<p className="no-products">No products found</p>
+// 			) : (
+// 				product.map((result, id) => (
+// 					<Link
+// 						to={`/product/product-details/${result._id}`}
+// 						className="result-item"
+// 						onClick={clearSearchInput}
+// 						key={id}
+// 					>
+// 						<img src={result.thumbnail} alt={result.name} />
+// 						<div className="result-info">
+// 							<h3>{result.name}</h3>
+// 							<p>₹{result.price}</p>
+// 						</div>
+// 					</Link>
+// 				))
+// 			)}
+// 		</div>
+// 	);
+// };
+
+// export default NavSearchList;
+
+
+
+
 import React from "react";
 import "./navSearchList.css";
 import { Link } from "react-router-dom";
@@ -38,6 +79,11 @@ import { Link } from "react-router-dom";
 const NavSearchList = ({ product, clearSearchInput, input, isLoading }) => {
 	return (
 		<div className="nav-search-list">
+			{/* Close Button */}
+			<button className="close-btn" onClick={clearSearchInput}>
+				&times;
+			</button>
+
 			{isLoading ? (
 				<div className="loading">Loading...</div>
 			) : input && product.length === 0 ? (
@@ -53,7 +99,8 @@ const NavSearchList = ({ product, clearSearchInput, input, isLoading }) => {
 						<img src={result.thumbnail} alt={result.name} />
 						<div className="result-info">
 							<h3>{result.name}</h3>
-							<p>₹{result.price}</p>
+							{/* <p>₹{result.price}</p> */}
+							<p>₹{result.size[0].FinalPrice}</p>
 						</div>
 					</Link>
 				))
